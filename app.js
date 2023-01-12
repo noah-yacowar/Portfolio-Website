3592793312665a574d067b2b3612418a74271832
Noah Yacowar <noahyacowar@gmail.com>
2023-01-12 16:59:32 -0500

    new changes

--- app.js
+++ app.js
@@ -3,6 +3,64 @@ const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
+const typedTextMainSpan = document.querySelector(".typed-text");
+const typedTextPreSpan = document.querySelector(".typed-text-prefix");
+const typedTextPerSpan = document.querySelector(".typed-text-period");
+let typedTextCurSpan=typedTextPreSpan;
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["A Software Developer.", "An Engineer.", "A Game Developer.", "A Web Designer."];
+const typingDelay = 200;
+const erasingDelay = 100;
+const loadDelay = 1500;
+const newWordDelay = 1100;
+const eraseCurWordDelay = 2000;
+    
+let textArrayIndex = 0;
+let charIndex = 0;
+let spaceIndex = 0;
+
+function type()
+{
+    
+    if(textArray[textArrayIndex].charAt(charIndex) == ' '  && typedTextCurSpan != typedTextMainSpan) 
+    {
+        typedTextCurSpan = typedTextMainSpan;
+        typedTextCurSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-2);
+        spaceIndex = charIndex;
+    }
+    
+    if(charIndex < textArray[textArrayIndex].length){
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextCurSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else{
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, eraseCurWordDelay);
+    }
+}
+
+function erase(){
+
+    if(spaceIndex == charIndex) typedTextCurSpan = typedTextPreSpan;
+
+    if(charIndex > 0){
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        if(typedTextCurSpan == typedTextMainSpan) typedTextCurSpan.textContent = textArray[textArrayIndex].substring(spaceIndex, charIndex-1);
+        else typedTextCurSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else{
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, newWordDelay);
+    }
+}
+
 function Submit(){
     document.getElementById("GFG").submit();
 }
@@ -46,3 +104,7 @@ function PageTransitions(){
 }
 
 PageTransitions();
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(textArray.length) setTimeout(type, loadDelay);
+}); 
